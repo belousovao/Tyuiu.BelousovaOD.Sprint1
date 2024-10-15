1c9a5a471c252c0b0de29d11031bc6511afc49c1
Tyuiu.BelousovaOD@outlook.com <bbb_od@mail.ru>
2024-10-16 02:29:59 +0500

    Task7

---
+++ Tyuiu.BelousovaOD.Sprint1.Task7.V5.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.BelousovaOD.Sprint1.Task7.V5.Lib;
+namespace Tyuiu.BelousovaOD.Sprint1.Task7.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = -5;
+            var res = ds.Calculate(x);
+            Assert.AreEqual(res, -0.387);
+        }
+    }
+}
---
+++ Tyuiu.BelousovaOD.Sprint1.Task7.V5.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BelousovaOD.Sprint1.Task7.V5.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BelousovaOD.Sprint1.Task7.V5.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e5ca738c-8fb5-442b-b2a1-7878364dc206")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
