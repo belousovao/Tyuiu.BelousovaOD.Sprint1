63fcc950f695509d92fb758a10862ca0dd07e39c
Tyuiu.BelousovaOD@outlook.com <bbb_od@mail.ru>
2024-10-15 18:02:14 +0500

    Task1

---
+++ Tyuiu.BelousovaOD.Sprint1.Task1.V13.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.BelousovaOD.Sprint1.Task1.V13.Lib
+{
+    public class DataService : ISprint1Task1V13
+    {
+        
+        public double Calculate(double x)
+        {
+            return x / (0.5 * x);
+        }
+    }
+}
+
---
+++ Tyuiu.BelousovaOD.Sprint1.Task1.V13.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BelousovaOD.Sprint1.Task1.V13.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BelousovaOD.Sprint1.Task1.V13.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9f57f014-61eb-455f-83f9-c06b91b57bf7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
