df76df5f0eb512aa8266b2b02f4281f287c3eb5d
Tyuiu.BelousovaOD@outlook.com <bbb_od@mail.ru>
2024-10-15 17:04:27 +0500

    Add project files.

---
+++ Tyuiu.BelousovaOD.Sprint1.Task0.V21.Test/DataServicTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.BelousovaOD.Sprint1.Task0.V21.Lib;
+namespace Tyuiu.BelousovaOD.Sprint1.Task0.V21.Test
+{
+    [TestClass]
+    public class DataServicTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(17, res);
+        }
+    }
+}
---
+++ Tyuiu.BelousovaOD.Sprint1.Task0.V21.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BelousovaOD.Sprint1.Task0.V21.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BelousovaOD.Sprint1.Task0.V21.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c477d718-582a-412c-8aa1-675a667ff208")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
